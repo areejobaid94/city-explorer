1461c7feff59afe865544af2688ad5eda4f1ee22
areejobaid94 <areej.obaid@yahoo.com>
2021-02-24 20:26:25 +0200

    done

--- server.js
+++ server.js
@@ -8,8 +8,7 @@ app.use(cors());
 require('dotenv').config();
 const port = process.env.PORT;
 const pg = require('pg');
-const client = new pg.Client({ connectionString: process.env.DATABASE_URL});
-
+const client = new pg.Client({ connectionString: process.env.DATABASE_URL,   ssl: { rejectUnauthorized: false } });
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
